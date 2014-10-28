97e8c6dbee59d5a20197dc7d7bf9df0e602ec154
Jonathan Lace <jlace@shp.org>
2014-10-28 18:15:50 -0400

    Committing early widget code...

---
+++ theologica/TodayViewController.h
@@ -0,0 +1,16 @@
+//
+//  TodayViewController.h
+//  Theologica
+//
+//  Created by Jonathan Lace on 10/27/14.
+//  Copyright (c) 2014 techrament. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *wordLabel;
+@property (strong, nonatomic) IBOutlet UILabel *definitionLabel;
+
+@end
